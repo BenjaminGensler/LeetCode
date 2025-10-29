2ae8f12ee47c844a14324e764fa67523571d7a01
Benjamin Gensler <117399625+BenjaminGensler@users.noreply.github.com>
2025-10-29 13:33:16 -0500

    Implement MyHashMap class with initial comment
    Added a class for designing a HashMap with an initial comment.

---
+++ Java/designHashMap.java
@@ -0,0 +1,35 @@
+/**
+* Benjamin Gensler
+* 10/29/2025
+* 706. Design HashMap
+*/
+class MyHashMap {
+    HashMap<Integer, Integer> map;
+
+    public MyHashMap() {
+        map = new HashMap<>();
+    }
+    
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+    
+    public int get(int key) {
+        if(map.get(key)==null){
+            return -1;
+        }
+        return map.get(key);
+    }
+    
+    public void remove(int key) {
+        map.remove(key);
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
