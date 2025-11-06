9beb084f1f72c79c81b82752e0e30ba8cc17017d
Benjamin Gensler <117399625+BenjaminGensler@users.noreply.github.com>
2025-11-06 11:54:29 -0600

    Simplify validWordSquare implementation
    Refactor validWordSquare method for clarity and efficiency.

--- Java/validWordSquare.java
+++ Java/validWordSquare.java
@@ -1,29 +1,26 @@
-// Benjamin Gensler
-// 3/7/25
-// 422. Valid Word Square
-// Determine from a List of Strings if the provided word square is read the same by column/ row numbers. (Ex/ [a,b], [b] -> True (ab = ab)     Ex2/ [a,b], [a] -> False ( ab != aa)
+/**
+* Benjamin Gensler
+* 11/06/2025
+* 422. Valid Word Square
+*/
 class Solution {
     public boolean validWordSquare(List<String> words) {
+        // Check if words[i][j] == words[j][i]
         for (int i = 0; i < words.size(); i++) {
-            // test string
-            String test = "";
-
-            // loop to create vertical test string
-            for (int k = 0; k < words.get(i).length() && k < words.size(); k++) {
-                // checks if the current word is longer than the current index
-                if (words.get(k).length() > i) {
-                    test += words.get(k).charAt(i);
-                } else {
+            String word = words.get(i);
+            for (int j = 0; j < word.length(); j++) {                
+                if (j >= words.size() || i >= words.get(j).length()){
+                    return false;
+                }
+                
+                // Compare characters
+                if (word.charAt(j) != words.get(j).charAt(i)) {
                     return false;
                 }
             }
-
-            // checks if test and the current word are the same
-            if (!test.equals(words.get(i))) {
-                return false;
-            }
         }
-
         return true;
+
+
     }
 }
